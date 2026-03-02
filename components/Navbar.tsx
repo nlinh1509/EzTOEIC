bd46ecfc9915cf80071bfaa6f7633470c3dbbef2
Lê Võ Nhật Linh <levonhatlinh.work@gmail.com>
2026-03-02 23:50:00 +0700

    done navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         {/* content in box */}
         <div className="flex justify-between items-center h-20">
-          <Link href="/" className="flex items-center gap-2 cursor-pointer ">
+          <Link href="/" className="flex items-center gap-2 ">
             <Image
               src="/logo.png"
               alt="strops logo"
@@ -23,12 +23,10 @@ export default function Header() {
           </Link>
 
           {/* search bar */}
-
-          {/* layout wrapper */}
-          <div className="hidden md:flex flex-1 max-w-xl mx-8">
+          <div className="hidden md:flex flex-1 max-w-xl mx-8"> {/* layout wrapper */}
             <div className="relative w-full   ">
               <input
-                className="block w-full pl-11 pr-4 py-2.5 bg-slate-100 dark:bg-slate-800 border border-transparent rounded-full focus:outline-none focus:border-primary focus:ring-4 focus:ring-primary/10 transition-all  "
+                className=" text-sm block w-full pl-11 pr-4 py-2.5 bg-slate-100 dark:bg-slate-800 border border-transparent rounded-full focus:outline-none focus:border-primary focus:ring-4 focus:ring-primary/10 transition-all  "
                 type="text"
                 placeholder="Search sports, venues, or locations..."
               />
@@ -36,11 +34,11 @@ export default function Header() {
           </div>
 
           {/* auth buttons */}
-          <div className="flex gap-4 ">
-            <button className="px-5 py-2 font-bold text-slate-700 dark:text-slate-300 hover:text-emerald-600 transition-colors cursor-pointer ">
-              Login
+          <div className="flex items-center gap-4 ">
+            <button className="px-5 py-2 text-sm font-bold text-slate-700 dark:text-slate-300  rounded-full hover:text-emerald-600 transition-colors cursor-pointer ">
+              Sign in
             </button>
-            <button className="px-6 py-2.5 font-bold bg-primary text-white rounded-full hover:bg-emerald-700 transition-colors shadow-md active:scale-95 cursor-pointer">
+            <button className="px-6 py-2.5 text-sm font-bold bg-primary text-white rounded-full hover:bg-emerald-700 transition-colors shadow-md active:scale-95 cursor-pointer">
               Sign up
             </button>
           </div>
